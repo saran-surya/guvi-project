a1e2dee07b391acf11e040a3a0a1a08fe93bf7fc
Saran Surya <saransurya199@gmail.com>
2021-01-25 22:50:31 +0530

    made final production changes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-// const path = require('path')
+const path = require('path')
+
 // middleware
 const cors = require('cors')
 const bodyParser = require('body-parser');
